19ba923dd3c0ab241f98f70cf79b7cb229e1b28d
md-anas-sabah <mdanassabah@gmail.com>
2025-10-10 12:34:29 +0530

    feat: Ignore venv directory

--- venv/lib/python3.10/site-packages/crewai/agents/__init__.py
+++
@@ -1,6 +0,0 @@
-from .cache.cache_handler import CacheHandler
-from .executor import CrewAgentExecutor
-from .parser import CrewAgentParser
-from .tools_handler import ToolsHandler
-
-__all__ = ["CacheHandler", "CrewAgentExecutor", "CrewAgentParser", "ToolsHandler"]
